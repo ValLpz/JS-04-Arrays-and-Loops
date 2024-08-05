dc0514b1c224e16f124cbbd3fbd891fe5929ed82
ValLpz <val.lpz.glz@gmail.com>
2024-08-05 02:18:41 -0600

    actualización 1, practica

---
+++ online.js
@@ -0,0 +1,15 @@
+let usuAn= (["mockIng99", "J0eyPunch", "glassedFer"]);
+    /*function userOn(usuAn) {
+        if (usuAn.length === 0) {
+            return "No hay usuarios en línea.";
+        }
+        else if (usuAn.length === 1) {
+            console.log(usuAn[0] + " está en línea.");
+        }
+        else if(usuAn.length <2) {
+            console.log(usuAn[0] + " y " + usuAn[1] +  (usuAn.length-2) + " más en línea.");
+        }
+    
+    }//Verificar por que no arroja nada en la consola*/
+
+console.log( usuAn[0] + " y " +usuAn[1] + " " + (usuAn.length-2) +" más en línea.");
